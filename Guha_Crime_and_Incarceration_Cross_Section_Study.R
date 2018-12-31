6638861b18394a5493616a1ba0f2a42a635ea9ee
Shukrit Guha <39396125+shukritg@users.noreply.github.com>
2018-12-31 02:58:04 -0500

    Add files via upload

--- Guha_Crime_and_Incarceration_Cross_Section_Study.R
+++ Guha_Crime_and_Incarceration_Cross_Section_Study.R
@@ -8,6 +8,7 @@ library(lmtest)
 library(xtable)
 library(stargazer)
 
+# Function for outputting regression tables
 xtable.coeftest <- function (x, caption = NULL, label = NULL, align =     NULL, digits = NULL, 
                              display = NULL, ...) 
 {
@@ -45,8 +46,7 @@ reg_1 = lm(log(Incarc_rate) ~ log(GDPPC) + log(Gini) + log(HSD) + Health + log(P
 summary(step(reg_1, direction = "backward"))
 reg_1 = lm(log(Incarc_rate) ~ Health + Conserv +log(Violent.Crime.Rate), data = Incarc_data)
 summary(reg_1)
-
-# health, conservative Advantage and Violent Crime
+# Health, Conservative Advantage and Violent Crime are significant.
 
 # It might be that Incarceration affects health outcomes, so reverse regress with health as dependent
 check_reg = lm(Health ~ log(GDPPC) + log(Gini) + log(HSD) + log(Incarc_rate) + log(Poverty) +log(LFP) + log(Unemp)
@@ -57,11 +57,13 @@ summary(step(check_reg, direction = "backward"))
 check_reg = lm(Health ~ log(Incarc_rate) + log(Poverty) + log(Unemp) + D_cont + Conserv + log(Violent.Crime.Rate),
                data = Incarc_data)
 summary(check_reg)
-# No, Incarceration is not significant in explaining Health. Moving on...
+# No, Incarceration is not significant in explaining Health. Only Poverty, Unemployment and the Conservative advantage
+# are significant (all at 1% level). Moving on..
 
-##
+####
 
-# Regress crime rates on everything
+# Regress crime rates on all explantory variables:
+
 # Violent Crime
 reg_2 = lm(log(Violent.Crime.Rate) ~ log(GDPPC) + log(Gini) + log(HSD) + Health + log(Poverty) +log(LFP) + log(Unemp)
                +log(V_part) +log(W_gap) + D_cont + G_cont + Corrup + Conserv +log(Religion), data = Incarc_data)
@@ -69,7 +71,7 @@ reg_2 = lm(log(Violent.Crime.Rate) ~ log(GDPPC) + log(Gini) + log(HSD) + Health 
 summary(step(reg_2, direction = "backward"))
 reg_2 = lm(log(Violent.Crime.Rate) ~ Health + Conserv + log(Religion), data = Incarc_data)
 summary(reg_2)
-# Health, Conservative advantage and Religion are significant
+# Health, Conservative advantage and Religion are significant.
 
 # Property Crime
 reg_3 = lm(log(Property.Crime.Rate) ~ log(GDPPC) + log(Gini) + log(HSD) + Health + log(Poverty) +log(LFP) + log(Unemp)
@@ -78,22 +80,23 @@ reg_3 = lm(log(Property.Crime.Rate) ~ log(GDPPC) + log(Gini) + log(HSD) + Health
 summary(step(reg_3, direction = "backward"))
 reg_3 = lm(log(Property.Crime.Rate) ~ log(LFP) + Corrup + G_cont + log(Religion) , data = Incarc_data)
 summary(reg_3)
-# LFP, Gun Control, Corruption and Religion
+# LFP, Gun Control, Corruption and Religion are significant.
 
 # All crime
 All_crime_reg = lm(log(Total.Crime.Rate) ~ log(GDPPC) + log(Gini) + log(HSD) + Health + log(Poverty) +log(LFP) + log(Unemp)
                +log(V_part) +log(W_gap) + D_cont + G_cont + Corrup + Conserv +log(Religion), data = Incarc_data)
 
 summary(step(third_reg, direction = "backward"))
-# Drug Control, Gun Control and Religion
-# Entering into Incarceration Rate Regressions
-# Found that backward stepwise selection only selects Violent Crime, not Property or Total Crime
+# Drug Control, Gun Control and Religion are significant.
 
-###
+####
 
-# So wait, Health is the most significant variable in explaining both violent crime and incarceration,
-# And Violent crime is significant in explaining incarceration.
-# Maybe health is endogenously affecting Violent Crime, and then through Violent Crime affecting Incarceration.
+cor(data.frame(Health, Incarc_rate, Violent.Crime.Rate))
+
+## NOTE:
+# Health is the most significant variable in explaining both violent crime and incarceration, but Violent crime 
+# is significant in explaining incarceration. Health is also highly correlated with both. 
+# Health maybe is endogenously affecting both Incarceration and Violent Crime.
 
 # Take health out of fourth_reg and find most significant variables
 reg_4 = lm(log(Incarc_rate) ~ log(GDPPC) + log(Gini) + log(HSD) + log(Poverty) +log(LFP) + log(Unemp)
@@ -103,33 +106,42 @@ reg_4 = lm(log(Incarc_rate) ~ log(GDPPC) + log(Gini) + log(HSD) + log(Poverty) +
 summary(step(reg_4, direction = "backward"))
 reg_4 = lm(log(Incarc_rate) ~ log(LFP) + Conserv + log(Violent.Crime.Rate), data = Incarc_data)
 summary(reg_4)
-# LFP replaces Health as significant variable
+# LFP replaces Health as significant variable.
 
 ############################### FIT INCARC MODEL AND TEST IV FOR VIOLENT CRIME ##########################################
 
-# Fit new model
-summary(lm(log(Incarc_rate) ~ log(LFP) +Conserv +log(Violent.Crime.Rate), data = Incarc_data))
+# From reg_2 we determined that health is the most significant variable in explaining Violent crime.
+# Thus, we want to test if Violent Crime is endogenous first, using Health as its instrument.
+
+# Revisit reg_4
+summary(reg_4)
+# Violent crime and labor force participation significant at 1% level. Conservative advantage at 0.1%.
 
 # Fit ivreg
 library(AER)
 
 ?ivreg
 
-summary(ivreg(log(Incarc_rate) ~ log(LFP) +Conserv +log(Violent.Crime.Rate) | log(LFP) +Conserv +Health, model = T,
-              data = Incarc_data), diagnostics = T)
-
 fit1 = ivreg(log(Incarc_rate) ~ log(LFP) +Conserv +log(Violent.Crime.Rate) | log(LFP) +Conserv +Health,
              data = Incarc_data, model = T)
 
+summary(fit1, diagnostics = T)
 # Weak instruments test infers that Health is a good instrument for Violent Crime (at 1% level)
-# The Wu-Hausman test signifies that violent Crime is endogenous, and IV estimator is consistent over OLS
-# Notice, that the significance of Violent Crime has reduced from the OLS model.
+# The Wu-Hausman test signifies that violent Crime is endogenous at 5% level - IV estimator is consistent over OLS
+# Notice, that the significance of Violent Crime has reduced from the OLS model i.e. from 1% to 5%.
+# The same is true for conservative advantage, which is now at 1% level of significance. The labor force
+# participation rate is no longer significant, even at the 10% level.
 
-# Also, the estimate has increased in value. Thus, the effect of Health (through Violent Crime) on the Incarceration
-# Rate is higher than that of Violent Crime by itself.  
+# Also notice that the coefficeint estimate for Violent Crime has increased in value. Thus, the effect of 
+# Health (through Violent Crime) on the Incarceration Rate is higher than that of Violent Crime by itself.
 
-## Note: We do not include Conserv as an instrument of Violent Crime despite its significance in explaining Violent
-# Crime (Equaltion (2)), because doing so leads to Overidentification. How? 
+# Possible economic interpretation:
+# Health outcomes may be indirectly aggravating imprisonment of offenders by increasing the risk of violent crimes. 
+# However, it may also be that health is itself endogenous. We investigate this in the next section.
+
+## NOTE: 
+# We do not include Conserv as an instrument of Violent Crime despite its significance in explaining Violent Crime 
+# (Equaltion (2)), because doing so leads to Overidentification. How? -
 # We first use step-wise selection to reselect the best variables to use after keeping Health and Conserv out.
 # We then use those variables + Violent crime in the model and instrument for Violent Crime using Health and Conserv
 # But doing so reveals a significant Sargan Test (The model is inferred as overidentified)
@@ -146,10 +158,12 @@ health_reg = lm(Health ~ log(GDPPC) + log(Gini) + log(HSD) + log(Poverty) +log(L
                 +log(V_part) +log(W_gap) + D_cont + G_cont + Corrup + Conserv +log(Religion), data = Incarc_data)
 
 summary(step(health_reg, direction = "backward"))
-# Poverty, Unemployment and Conservative advantage (Already in)
+# Poverty, Unemployment and Conservative Advantage.
+
 reg_5 = lm(Health ~ log(Poverty) + log(Unemp) + Conserv, data = Incarc_data)
 
-# Assuming Health is Endogenous, we fit the ivreg instrumenting Health with Poverty and Unemployment
+# Assuming Health is Endogenous, we fit the ivreg instrumenting Health with all three significant variables i.e.
+# Poverty, Unemployment and the Conservative Advantage. 
 library(AER)
 summary(lm(log(Incarc_rate)~ log(Violent.Crime.Rate)+Health +Conserv, data = Incarc_data))
 
@@ -157,7 +171,8 @@ fit2 = ivreg(log(Incarc_rate) ~ log(Violent.Crime.Rate) +Health +Conserv | log(V
                      +log(Poverty) + log(Unemp), data = Incarc_data)
 
 summary(fit2, diagnostics = T)
-# They are good instruments; no endogeneity or overidentification so OLS is better than IV, health remains significant!
+# They are good instruments; no endogeneity or overidentification so OLS is better than IV, but 
+# health remains significant at the 0.1% level!
 
 ###################### DIAGNOSTIC TEST RESULTS ##############################################
 
@@ -194,7 +209,7 @@ xtable.coeftest(t)
 
 # Autocorrelation in errors
 dwtest(Incarc_reg)
-# p-value of 0.3385, fail to reject null of normally distributed errors
+# p-value of 0.3385, fail to reject null of normally distributed errors. 
 
 # Multicollinearity
 vif(reg_1)
@@ -202,6 +217,7 @@ vif(reg_2)
 vif(reg_3)
 vif(reg_4)
 vif(reg_5)
+# No serious multicollineariy
 
 ######################################## CONCLUSIONS ######################################################
 
@@ -209,13 +225,13 @@ vif(reg_5)
 # In the first section, we suspected health to be influencing Incarceration through its effect on Violent Crime because
 # Health was most significant in explaining both - through best subset selection. Our results indicated that Violent
 # Crime truly was endogenous, and that Health was a good instrument for Violent Crime. In the IV regression,
-# the significance of Violent Crime and Conserv are both reduced by the inclusion of Health as an isntrument, but the
+# the significance of Violent Crime and Conserv are both reduced by the inclusion of Health as an instrument, but the
 # estimates for Violent crime increases by more than twofold (from ~0.29 in OLS to ~0.77 in IV).
 
-
-# We fitted a linear model specified by fourth_reg to obtain the same results as the subset selection. However, 
-# applying the Breusch-Pagan test to our fitted model, we found hetereoscedatic errors. Correcting for them and testing 
-# the coefficients again for significance revealed that conserv and Violent Crime dropped out, leaving only Health behind.
+# We fitted a linear model specified by reg_1 to obtain the same results as the subset selection. However, 
+# applying the Breusch-Pagan test to our fitted model, we found hetereoscedatic errors. Correcting for them and 
+# testing the coefficients again for significance revealed that conserv and Violent Crime dropped out, leaving 
+# only Health behind.
 
 # Lastly, we suspected that health might be an endogenous variable itself - such that other factors might be influencing
 # both health and incarceration, making it look like health is affecting incarceration in some way.
